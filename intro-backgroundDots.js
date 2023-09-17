7d1bd73f4dba3c582abe0842064bc800ea986ca4
Eric Jiahuan Cheng <jc11769@nyu.edu>
2023-09-17 10:56:51 -0400

    rewrite intro background dot code

--- intro-backgroundDots.js
+++ intro-backgroundDots.js
@@ -2,39 +2,43 @@ const dotSize_intro = 3
 const spacing_intro = dotSize_intro * 8
 const areaOfEffect_intro = 72
 
-let dots_intro = []
+// const dotSize_intro = 3
+// const spacing_intro = dotSize_intro * 8
+// const areaOfEffect_intro = 72
 
-function setup() {
-  let cnv = createCanvas(windowWidth, windowHeight);
-  cnv.parent("intro-canvas-container");
-  for (let k = 0; k < width; k += spacing_intro) {
-    for (let l = 0; l < height; l += spacing_intro) {
-      dots_intro.push(new Dot_intro(i + spacing_intro/2, j + spacing_intro/2, dotSize_intro))
-    }
-  }
-  noStroke()
-}
+// let dots_intro = []
 
-function draw() {
-  background(235, 235, 240);
-  dots_intro.forEach(dots_intro => {
-    dots_intro.update()
-    dots_intro.render()
-  })
-}
+// function setup() {
+//   let cnv = createCanvas(windowWidth, windowHeight);
+//   cnv.parent("intro-canvas-container");
+//   for (let k = 0; k < width; k += spacing_intro) {
+//     for (let l = 0; l < height; l += spacing_intro) {
+//       dots_intro.push(new Dot_intro(i + spacing_intro/2, j + spacing_intro/2, dotSize_intro))
+//     }
+//   }
+//   noStroke()
+// }
 
-class Dot_intro {
-  constructor(x, y, size) {
-    this.x = x;
-    this.y = y;
-    this.size = size;
-    this.transparency = 40
-  }
+// function draw() {
+//   background(235, 235, 240);
+//   dots_intro.forEach(dots_intro => {
+//     dots_intro.update()
+//     dots_intro.render()
+//   })
+// }
+
+// class Dot_intro {
+//   constructor(x, y, size) {
+//     this.x = x;
+//     this.y = y;
+//     this.size = size;
+//     this.transparency = 40
+//   }
   
-  render() {
-    fill(255, this.transparency)
+//   render() {
+//     fill(255, this.transparency)
     
-    ellipse(this.x, this.y, this.size)
-  }
-}
+//     ellipse(this.x, this.y, this.size)
+//   }
+// }
 
