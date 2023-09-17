b719c7701e7c14cdf350daf5679a973158907f36
Eric Jiahuan Cheng <jc11769@nyu.edu>
2023-09-17 17:02:43 -0400

    v13.1 transparent background

--- main-backgroundDots-test13.1.js
+++ main-backgroundDots-test13.1.js
@@ -27,7 +27,7 @@ let h = p5Container.clientHeight;
  }
 
  function draw() {
-   background(36, 36, 38);
+   //background(36, 36, 38);
    dots.forEach(dot => {
      dot.update()
      dot.render()
