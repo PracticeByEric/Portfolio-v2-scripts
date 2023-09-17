4fa6255ec5638f6616da14f03fe104698877e768
Eric Jiahuan Cheng <jc11769@nyu.edu>
2023-09-17 13:50:16 -0400

    move back

--- projectCards-v1.js
+++ projectCards-v1.js
@@ -371,31 +371,30 @@ $("[jc-portfolio='homepage-main-wrapper']").each(function (index) {
     );
   });
   
-  
   // BG color change start from 'homepage-main-wrapper'
-  $("homepage-main-wrapper").each(function (index) {
-    let triggerElement = $(this);
-    let targetElement = $("body");
-  
-    let tl = gsap.timeline({
-      scrollTrigger: {
-        trigger: triggerElement,
-  
-        start: "top top",
-        end: "bottom bottom",
-        scrub: 1
-      }
-    });
-    tl.fromTo(targetElement, {
-        backgroundColor: "#000000",
-      },
-      {
-         backgroundColor: "#242426",
-        duration: 1
-      }
-    );
+$("homepage-main-wrapper").each(function (index) {
+  let triggerElement = $(this);
+  let targetElement = $("body");
+
+  let tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: triggerElement,
+
+      start: "top top",
+      end: "bottom bottom",
+      scrub: 1
+    }
   });
-  
+  tl.fromTo(targetElement, {
+      backgroundColor: "#000000",
+    },
+    {
+       backgroundColor: "#242426",
+      duration: 1
+    }
+  );
+});
+
   // dot-bg opacity
   $("[jc-portfolio='homepage-main-wrapper']").each(function (index) {
     let triggerElement = $(this);
