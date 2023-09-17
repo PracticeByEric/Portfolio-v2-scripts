a96c4beb86d6c13766c2422fc0915e9b7566e680
Eric Jiahuan Cheng <jc11769@nyu.edu>
2023-09-17 13:47:37 -0400

    seperate bg color change gradient v1

--- projectCards.js
+++ projectCards.js
@@ -372,7 +372,7 @@ $("[jc-portfolio='homepage-main-wrapper']").each(function (index) {
   });
   
   
-  // BG color change
+  // BG color change start from 'homepage-main-wrapper'
   $("homepage-main-wrapper").each(function (index) {
     let triggerElement = $(this);
     let targetElement = $("body");
