76fb6ab6963b73a205bd83abbbcdf182257f1773
Eric Jiahuan Cheng <jc11769@nyu.edu>
2023-09-30 10:28:19 -0400

    adjust bounding box and gap

--- projectCards-v11.js
+++ projectCards-v11.js
@@ -23,11 +23,11 @@ $("[jc-portfolio='homepage-main-wrapper']").each(function (index) {
     // Gap original from 10px
     tl.fromTo(targetElement, {
           // old 100px
-          width: "150px",
+          width: "100px",
           // old 100px
-        height:"150px",
+        height:"100px",
         // old 10px
-        gap: "16px",
+        gap: "12px",
       },
       // Sizing to 426px height
       // Gap to 16px
