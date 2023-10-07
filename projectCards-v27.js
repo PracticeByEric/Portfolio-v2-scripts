bd896aab6c94f2ebe671e9ce290788aedc7ae8e4
Eric Jiahuan Cheng <jc11769@nyu.edu>
2023-10-07 14:25:45 -0400

    change window back to 65vh

--- projectCards-v27.js
+++ projectCards-v27.js
@@ -33,8 +33,8 @@ $("[jc-portfolio='homepage-main-wrapper']").each(function (index) {
       // Gap to 16px
       {
         gap: "16px",
-        width: "70vh",
-        height:"70vh",
+        width: "65vh",
+        height:"65vh",
         duration: 1,
       }
     );
