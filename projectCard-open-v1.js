e419b535a2b6528c72abddd5593e2b8d820f7830
Eric Jiahuan Cheng <jc11769@nyu.edu>
2023-10-07 17:56:59 -0400

    add project card 3 open

--- projectCard-open-v1.js
+++ projectCard-open-v1.js
@@ -1,5 +1,13 @@
-const cardToOpen = document.getElementById('projectCard-2');
-  cardToOpen.addEventListener('click', (e) => {
-  	cardToOpen.classList.toggle("open-card-2");
-    cardToOpen.log("clicked");
+// click to open project card 2
+const card2Open = document.getElementById('projectCard-2');
+  card2Open.addEventListener('click', (e) => {
+  	card2Open.classList.toggle("open-card-2");
+    card2Open.log("clicked");
+  })
+
+  // click to open project card 3
+  const card3Open = document.getElementById('projectCard-3');
+  card3Open.addEventListener('click', (e) => {
+  	card3Open.classList.toggle("open-card-3");
+    card3Open.log("clicked");
   })
