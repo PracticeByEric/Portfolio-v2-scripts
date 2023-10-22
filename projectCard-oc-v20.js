842ba62a8995ba54b1c2517d18f9153314e06718
Eric Jiahuan Cheng <jc11769@nyu.edu>
2023-10-21 23:09:43 -0400

    add toggle ease?

--- projectCard-oc-v20.js
+++ projectCard-oc-v20.js
@@ -2,7 +2,7 @@
 const cardGrid2 = document.getElementById('card-column-2');
 // hover rise card
 cardGrid2.addEventListener('mouseenter', (e)=>{
-  cardGrid2.classList.toggle('rise');
+  cardGrid2.classList.toggle(300,'rise');
 })
 cardGrid2.addEventListener('mouseleave', (e)=>{
   cardGrid2.classList.remove('rise');
