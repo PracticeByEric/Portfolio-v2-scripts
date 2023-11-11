5a288a39400049a1bb27a254d67fc8230f22ce61
Eric Jiahuan Cheng <jc11769@nyu.edu>
2023-11-11 18:18:55 -0500

    add project 1 open

---
+++ project-detail-open.js
@@ -0,0 +1,8 @@
+// Click button to open layer 3
+const card2Button = document.getElementById("card-2-open-button");
+const project2Detail = document.getElementById("test-project-1-scrolling-div");
+
+// card 2 button to open project 2 detail
+card2Button.addEventListener("click", function(){
+    project2Detail.style.display = "block";
+})
