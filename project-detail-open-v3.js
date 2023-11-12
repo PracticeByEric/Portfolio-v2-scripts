387e0471cb4a47f0964c0130d4efdcdc10f2f915
Eric Jiahuan Cheng <jc11769@nyu.edu>
2023-11-11 20:42:57 -0500

    hide project card on layer 2

--- project-detail-open-v3.js
+++ project-detail-open-v3.js
@@ -3,6 +3,8 @@ const card2Button = document.getElementById("card-2-open-button");
 const project2Detail = document.getElementById("test-project-1-scrolling-div");
 // get project card layer 3
 const project2CardLayer3 = document.getElementById("project-card-layer-3");
+// get project card layer 2
+const project2CardLayer2 = document.getElementById("projectCard-2-open");
 
 // card 2 button to open project 2 detail
 card2Button.addEventListener("click", function(){
@@ -12,4 +14,6 @@ card2Button.addEventListener("click", function(){
     project2CardLayer3.style.display = "flex";
     // add card to banner tag to project 2 card layer 3
     project2CardLayer3.classList.toggle("card-to-banner");
+    // hide project card on layer 2
+    project2CardLayer2.style.display = "none";
 })
