c8f933c21fde5ed5206c9835aed785ef33b9b96b
Eric Jiahuan Cheng <jc11769@nyu.edu>
2023-11-12 12:19:55 -0500

    note 4.1

--- project-detail-open-p4.1.js
+++ project-detail-open-p4.1.js
@@ -111,3 +111,42 @@ card4Button.addEventListener("click", function(){
         // with delay
       }, 750); // time in milliseconds
 })
+
+// =======================>>>> OPEN PROJECT 4 DETAIL <<<========================================================================
+// Get button on project card 2(for project 1)
+const card5Button = document.getElementById("card-5-open-button");
+// Get project 1 scrolling div
+const project4ScrollingDiv = document.getElementById("project4-scrolling-div");
+
+// Get project card 2 on layer 3
+const card5Layer3 = document.getElementById("card5-layer3");
+
+// Get project card 2 on layer 2
+const card5Layer2 = document.getElementById("projectCard-5-open");
+
+// Get project 1 content detail
+const project4Detail = document.getElementById("project4-detail");
+
+// card 2 button to open project 2 detail
+card5Button.addEventListener("click", function(){
+    console.log("scroll div trigger");
+    // display project detail page with flex layout
+    project4ScrollingDiv.style.display = "flex";
+    // display project card layer 3
+    card5Layer3.style.display = "flex";
+    // add card to banner tag to project 2 card layer 3
+    card5Layer3.classList.toggle("card-to-banner");
+    // hide project card on layer 2
+    card5Layer2.style.display = "none";
+    // show project 2 detail after few seconds of delay
+    setTimeout(() => {
+      
+        // removes element from DOM
+        // show project detail body
+        project4Detail.style.display = "flex";
+      
+        // hides element (still takes up space on page)
+        // box.style.visibility = 'hidden';
+        // with delay
+      }, 750); // time in milliseconds
+})
