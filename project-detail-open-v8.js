500a68859d6bed8964664f63261f9c241dc56bd8
Eric Jiahuan Cheng <jc11769@nyu.edu>
2023-11-12 11:39:14 -0500

    rename all variable names for project 1

--- project-detail-open-v8.js
+++ project-detail-open-v8.js
@@ -1,22 +1,25 @@
-// Click button to open layer 3
+// Get button on project card 2(for project 1)
 const card2Button = document.getElementById("card-2-open-button");
 // Get project 1 scrolling div
 const project1ScrollingDiv = document.getElementById("project1-scrolling-div");
-// get project card layer 3
-const project2CardLayer3 = document.getElementById("project-card-layer-3");
-// get project card layer 2
+
+// Get project card 2 on layer 3
+const card2Layer3 = document.getElementById("card2-layer3");
+
+// Get project card 2 on layer 2
 const project2CardLayer2 = document.getElementById("projectCard-2-open");
-// get project card page detail
-const project2DetailBody = document.getElementById("test-project-1-detail");
+
+// Get project 1 content detail
+const project1Detail = document.getElementById("project1-detail");
 
 // card 2 button to open project 2 detail
 card2Button.addEventListener("click", function(){
     // display project detail page with flex layout
     project1ScrollingDiv.style.display = "flex";
     // display project card layer 3
-    project2CardLayer3.style.display = "flex";
+    card2Layer3.style.display = "flex";
     // add card to banner tag to project 2 card layer 3
-    project2CardLayer3.classList.toggle("card-to-banner");
+    card2Layer3.classList.toggle("card-to-banner");
     // hide project card on layer 2
     project2CardLayer2.style.display = "none";
     // show project 2 detail after few seconds of delay
@@ -24,7 +27,7 @@ card2Button.addEventListener("click", function(){
       
         // 👇️ removes element from DOM
         // show project detail body
-        project2DetailBody.style.display = "flex";
+        project1Detail.style.display = "flex";
       
         // 👇️ hides element (still takes up space on page)
         // box.style.visibility = 'hidden';
