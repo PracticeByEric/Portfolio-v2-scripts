b8a1cf449960136c58093c76c00c3257a20a1293
Eric Jiahuan Cheng <jc11769@nyu.edu>
2023-11-14 15:40:08 -0500

    audit div overlap with detail product div

---
+++ project1-timeline-trigger-v16.js
@@ -0,0 +1,29 @@
+// project1-scrolling-div
+
+const project1Scroll = document.getElementById("project1-scrolling-div");
+
+const productTitle = document.getElementById("project1-timeline-audit-products-title");
+
+function triggerSectionPos(){
+    const auditTrigger = document.getElementById("project1-timeline-audit-trigger");
+    const productDetail = document.getElementById("project1-timeline-audit-products-detail");
+
+    const auditRect = auditTrigger.getBoundingClientRect();
+    const pdRect = productDetail.getBoundingClientRect();
+    
+    // if(auditRect.top < pdRect.top){
+    //     console.log("OUTSIDE")
+    // } else{
+    //   console.log("EXCEEDED");  
+    //   // show product Title div
+    //   productTitle.style.display = 'block';
+    // }
+
+    if(auditRect.top > pdRect.top && auditRect.bottom < pdRect.bottom){
+        console.log("INTERSECT!");
+        productTitle.style.display = 'block';
+    }
+}
+
+project1Scroll.addEventListener("scroll", triggerSectionPos);
+triggerSectionPos();
