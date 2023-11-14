cb4a0d37b9cdb9a7334e5e2e4d4b5d662634da27
Eric Jiahuan Cheng <jc11769@nyu.edu>
2023-11-14 17:41:18 -0500

    added for define section

--- project1-timeline-trigger-10.js
+++ project1-timeline-trigger-10.js
@@ -1,14 +1,22 @@
 // Timeline trigger based scroll
 const project1Scroll = document.getElementById("project1-scrolling-div");
 
-// Ttiles in SECTION 1: AUDIT
+// ==============================>> TITLES FROM EACH SECTION<<===================================
+// Titles in SECTION 1: AUDIT
 const titleProduct = document.getElementById("project1-timeline-audit-products-title");
 const titlePatterns = document.getElementById("project1-timeline-audit-patterns-title");
 const titleReferences = document.getElementById("project1-timeline-audit-references-title");
+// Titles in SECTION 2: DEFINE
+const titleUX = document.getElementById("project1-timeline-define-ux-title");
+const titleVisual = document.getElementById("project1-timeline-define-visual-title");
+// Title in SECTION 3
+// Title in SECTION 4
+// Title in SECTION 5
 
-// set initial opacity for all titles
-let opacity = 0;
+// // set initial opacity for all titles
+// let opacity = 0;
 
+// ========================>> SECTION 1: AUDIT<<====================================================
 // detecting audit trigger position in relation to each section
 function auditTriggerSectionPos(){
     // Get audit trigger
@@ -72,6 +80,43 @@ function auditTriggerSectionPos(){
     }else{
         console.log("OUTSIDE REFERENCES!");
         titleReferences.style.display = 'none';
+    }
+}
+
+//===============================>> SECTION 2: DEFINE CRITERIA<<===============================
+function defineTriggerSectionPos(){
+    //  Get define trigger
+    const defineTrigger = document.getElementById("project1-timeline-define-trigger");
+    // Get define trigger div dimension
+    const defineRect = defineTrigger.getBoundingClientRect();
+
+    // Get each detail section in define
+    const uxDetail = document.getElementById("project1-timeline-define-ux-detail");
+    const visualDetail = document.getElementById("project1-timeline-define-visual-detail");
+    // Get each detail div dimension
+    const uxRect = uxDetail.getBoundingClientRect();
+    const visualRect = visualDetail.getBoundingClientRect();
+
+    // STAGE ONE: Show ux title in ux section
+    if(defineRect.top > uxRect.top && defineRect.bottom < uxRect.bottom){
+        console.log("INTERSECT UX!");
+
+        // show ux title
+        titleUX.style.display = 'block';
+    }else{
+        console.log("OUTSIDE UX!");
+        titleUX.style.display = 'none';
+    }
+    // STAGE TWO: Show visual tile in visual section
+
+    if(defineRect.top > visualRect.top && defineRect.bottom < visualRect.bottom){
+        console.log("INTERSECT VISUAL!");
+
+        // show visual title
+        titleVisual.style.display = 'block';
+    }else{
+        console.log("OUTSIDE VISUAL!");
+        titleVisual.style.display = 'none';
     }
 }
 
