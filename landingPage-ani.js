3ae5b6ddcab721d967e6e476de594de59538ed30
Eric Jiahuan Cheng <jc11769@nyu.edu>
2023-11-17 09:19:12 -0500

    test with position printing

---
+++ landingPage-ani.js
@@ -0,0 +1,17 @@
+// import GSAP plugin
+gsap.registerPlugin(MotionPathPlugin);
+
+//=============================>>LOCATING START & END LOCATION<<====================================
+const hiWrap = document.getElementById("landing-text-wrap-hi");
+const designerWrap = document.getElementById("landing-text-wrap-designer");
+
+const hiRect = hiWrap.getBoundingClientRect();
+const designerRect = designerWrap.getBoundingClientRect();
+
+function printWrapPos(){
+    console.log(hiRect.right + "," + hiRect.bottom);
+    console.log(designerRect.right + "," + designerRect.top);
+}
+
+project1Scroll.addEventListener("scroll", printWrapPos);
+printWrapPos();
