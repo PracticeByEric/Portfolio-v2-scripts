b6647f87c79c45313b0e660ba8c3e9c2228e8f65
Eric Jiahuan Cheng <jc11769@nyu.edu>
2023-11-17 14:35:24 -0500

    v9

--- landingPage-ani-v9.js
+++ landingPage-ani-v9.js
@@ -2,6 +2,7 @@
 gsap.registerPlugin(MotionPathPlugin);
 
 // FOR INTRO, IT IS WINDOW SCROLLING INSTEAD OF DIV
+const testSVG = document.getElementById("test-svg-block");
 
 //=============================>>LOCATING START & END LOCATION<<====================================
 const hiWrap = document.getElementById("landing-text-wrap-hi");
@@ -14,6 +15,10 @@ function printWrapPos(){
     console.log(hiRect.right + "," + hiRect.bottom);
     console.log(designerRect.right + "," + designerRect.top);
 }
+
+testSVG.style.position = 'absolute';
+testSVG.style.left = hiRect.right;
+testSVG.style.top = designerRect.bottom;
 
 window.addEventListener("scroll", printWrapPos);
 printWrapPos();
