718b1cb69cc7a5d5a6b7aa4a881bf88654fcc628
Eric Jiahuan Cheng <jc11769@nyu.edu>
2023-11-19 14:40:04 -0500

    20

--- landingpage-underScore19.js
+++
@@ -1,5 +0,0 @@
-gsap.registerPlugin(DrawSVGPlugin);
-
-console.log("IMPORTED");
-
-gsap.to("#sinePath", {duration:1,drawSVG: "0% 100%"});
---
+++ landingpage-underScore20.js
@@ -0,0 +1,5 @@
+gsap.registerPlugin(DrawSVGPlugin);
+
+gsap.to("#sinePath", {duration:1,drawSVG: "0% 100%"});
+
+console.log("EXCUTED!");
