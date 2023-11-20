fdff9c7a31b94ad70003cd25e9dead87196e057f
Eric Jiahuan Cheng <jc11769@nyu.edu>
2023-11-19 21:06:38 -0500

    v4

--- landing-product-ani-v4.js
+++ landing-product-ani-v4.js
@@ -24,9 +24,15 @@ var action = gsap.timeline({defaults: {duration: 1, ease: 'none'},
 }
 )
 
-.fromTo("#sinePath", {drawSVG: "0 0%"}, {duration:1,drawSVG: "0% 100%", onComplete: ()=>{
-  // once the drawSVG function finish
-  console.log('Completed!');
-  // Trigger animation
-  testFlower.classList.toggle("flowerGrow");
+// .fromTo("#sinePath", {drawSVG: "0 0%"}, {duration:1,drawSVG: "0% 100%", onComplete: ()=>{
+//   // once the drawSVG function finish
+//   // console.log('Completed!');
+//   // Trigger animation
+//   // testFlower.classList.toggle("flowerGrow");
+// }});
+
+
+.fromTo("#sinePath", {drawSVG: "0 0%"}, {duration:1,drawSVG: "0% 100%", onUpdate: ()=>{
+  const percentageProgressed = Math.floor(DrawSVGPlugin.getPosition("#sinePath")[1] / (DrawSVGPlugin.getLength("#sinePath") / 100));
+  console.log(percentageProgressed);
 }});
