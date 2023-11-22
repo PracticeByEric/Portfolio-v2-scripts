66c4ba38954d22cb491881efde952a48776f72f9
Eric Jiahuan Cheng <jc11769@nyu.edu>
2023-11-22 16:10:26 -0500

    v6

---
+++ landingPage-paradox-ani-v6.js
@@ -0,0 +1,23 @@
+gsap.registerPlugin(TextPlugin);
+gsap.registerPlugin(ScrollTrigger);
+
+let textElem = document.getElementById("real-world-paradox-text");
+const timePerCharacter = 0.02;
+
+gsap.to(".real-world-paradox-text-wrap",{
+        ScrollTrigger:{
+            trigger: ".real-world-paradox-text-wrap",
+            toggleActions: "play",
+            start: "top 90%",
+            end: "center top",
+            scrub: true,
+            marker: true
+        },
+        text:{
+            value: textElem[0].innerHTML
+        },
+        duration: textElem[0].innerText.length * timePerCharacter,
+        delay: 0.5,
+        ease: "none",
+        delimiter: ""
+    })
