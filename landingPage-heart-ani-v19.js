415b60cba8f733a0c66decabbadb45217dfb32c3
Eric Jiahuan Cheng <jc11769@nyu.edu>
2023-11-26 11:53:24 -0500

    v19

--- landingPage-heart-ani-v19.js
+++ landingPage-heart-ani-v19.js
@@ -2,12 +2,13 @@ const heartTextDiv = document.getElementById("heart-text-div");
 
 const heartSpiralDiv = document.getElementById("test-heart-spiral-div");
 
+const pageScrollTop = window.scrollY;
 
 var heartDivLeft = heartTextDiv.getBoundingClientRect().left;
 var heartDivBottom = heartTextDiv.getBoundingClientRect().top;
 
 heartSpiralDiv.style.position = "absolute";
 heartSpiralDiv.style.left = heartDivLeft +'px';
-heartSpiralDiv.style.bottom = heartDivBottom + document.body.scrollTop() + 'px';
+heartSpiralDiv.style.bottom = heartDivBottom + pageScrollTop + 'px';
 
 console.log(document.body.scrollTop());
