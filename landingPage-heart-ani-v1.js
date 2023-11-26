6b649a4c22a51a3bde46f7e8dc72f1ee2e2aae8e
Eric Jiahuan Cheng <jc11769@nyu.edu>
2023-11-26 11:06:45 -0500

    v2

--- landingPage-heart-ani-v1.js
+++
@@ -1,3 +0,0 @@
-const heartTextDiv = document.getElementById("heart-text-div");
-
-console.log(heartTextDiv.getBoundingClientRect.left);
---
+++ landingPage-heart-ani-v2.js
@@ -0,0 +1,5 @@
+const heartTextDiv = document.getElementById("heart-text-div");
+
+var heartDivLeft = heartTextDiv.getBoundingClientRect.left;
+
+console.log(heartDivLeft);
