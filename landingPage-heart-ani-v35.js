b0ae8efc3d60f89de4392ac54051abe498948cf7
Eric Jiahuan Cheng <jc11769@nyu.edu>
2023-11-26 12:49:03 -0500

    350

--- landingPage-heart-ani-v35.js
+++ landingPage-heart-ani-v35.js
@@ -6,6 +6,6 @@ console.log(heartTextDiv.offsetTop);
 heartTextDivLeft = heartTextDiv.getBoundingClientRect().left;
 
 heartSpiralDiv.style.position = "absolute";
-heartSpiralDiv.style.left = heartTextDivLeft + "550" +"px";
+heartSpiralDiv.style.left = heartTextDivLeft + 550 + "px";
 heartSpiralDiv.style.top = heartTextDiv.offsetTop + "px";
 
