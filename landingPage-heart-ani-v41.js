ac762b5357060aef8936c4435537ec8c49516ce9
Eric Jiahuan Cheng <jc11769@nyu.edu>
2023-11-26 13:36:50 -0500

    41

--- landingPage-heart-ani-v41.js
+++ landingPage-heart-ani-v41.js
@@ -1,10 +1,21 @@
+gsap.registerPlugin(DrawSVGPlugin);
+
+const heartTextDiv = document.getElementById("heart-text-div");
+const heartSpiralDiv = document.getElementById("test-heart-spiral-div");
+
+console.log(heartTextDiv.offsetTop);
+
+heartTextDivLeft = heartTextDiv.getBoundingClientRect().left;
+
+heartSpiralDiv.style.position = "absolute";
+heartSpiralDiv.style.left = heartTextDivLeft + 600 + "px";
+heartSpiralDiv.style.top = heartTextDiv.offsetTop + "px";
+
+//=======================>> EXPLODE <<========================
 gsap.registerPlugin(ScrollTrigger);
 gsap.registerPlugin(Physics2DPlugin);
 
-var emitter = document.getElementById("test-emitter");
-
-const buckleTextDiv = document.getElementById("buckle-up-d1");
-const buckleTextTrigger = document.getElementById("buckle-trigger");
+var emitter = document.getElementById("test-emitter-2");
 
 container = document.createElement("div-2"),
 
@@ -69,28 +80,15 @@ function getRandom(min, max){
     return min + Math.random() * (max - min);
 }
 
-// explode(emitter);
+//========================================================================
 
-// mouse click to trigger emitter
-// emitter.onmousedown = emitter.ontouchstart = function(){
-//     console.log("CLICKED!");
-//     explode(emitter);
-// }
 
-const buckleText = document.getElementById("buckle-text");
 
-gsap.fromTo(buckleTextDiv,{scale:5, opacity:0}, {scale: 1, opacity: 1, scrollTrigger: {
-    trigger: buckleTextTrigger,
-    start: "top center+=50px",
+gsap.fromTo("#heart-spiral-curve", {drawSVG: "100% 100%"}, {duration: 1.5, drawSVG: "0% 100%",
+scrollTrigger: {
+    trigger: heartTextDiv,
+    start: "top bottom-=100px",
     end: "bottom center",
-    markers:false,
-    scrub: true
-    }, onComplete: ()=>{
-        console.log("BUCKLE COMPLETE!");
-        explode(emitter);
-        buckleText.classList.add("buckle-active");
-        buckleText.classList.remove("buckle-inactive");
-    }, onUpdate: ()=>{
-        buckleText.classList.remove("buckle-active");
-        buckleText.classList.add("buckle-inactive");
-    }})
+    scrub: true,
+    markers: true
+  }});
