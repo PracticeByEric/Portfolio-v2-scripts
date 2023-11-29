1cd4f13f63f2a285ce5ce104c5fceb4f316f1895
Eric Jiahuan Cheng <jc11769@nyu.edu>
2023-11-28 20:54:58 -0500

    greeting v4

--- landingPage-greeting-v4.js
+++ landingPage-greeting-v4.js
@@ -5,9 +5,9 @@ var timeOutput;
 if(curHr < 12){
     timeOutput = 'Morning,';
 }else if(curHr < 18){
-    timeOutput = 'Hi there,';
+    timeOutput = 'Good afternoon,';
 }else{
-    timeOutput = 'Hello,'
+    timeOutput = 'Good night,'
 }
 
 var typed = new Typed(".typedjs-simple", {
