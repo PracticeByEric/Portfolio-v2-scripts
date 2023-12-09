1c2f29dcc5719686505f775a5fee09d84b0d517e
Eric Jiahuan Cheng <jc11769@nyu.edu>
2023-12-09 15:34:49 -0500

    11

---
+++ landingPage-hello-ani-v11.js
@@ -0,0 +1,15 @@
+gsap.registerPlugin(DrawSVGPlugin);
+var spiralComplete = false;
+
+const wavingHand = document.getElementById("waving-hand");
+
+gsap.fromTo("#helloSpiralPath", 
+    {drawSVG: "0 0%", opacity: 0}, 
+    {duration: 1.25, drawSVG: "0% 100%", opacity: 1, delay:1.25, onComplete: ()=> {
+    // buckleText.classList.add("buckle-active");
+    wavingHand.classList.add("popUp-wave-ani-group");
+} });
+
+document.addEventListener('DOMContentLoaded', function() {
+    console.log("Page finish loading");
+ }, false);
