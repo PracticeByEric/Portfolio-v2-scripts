5b65fde0888493394051da2b89ed1877add71558
Eric Jiahuan Cheng <jc11769@nyu.edu>
2023-12-09 15:18:24 -0500

    change wave drawing speed

--- landingPage-hello-ani-v7.js
+++ landingPage-hello-ani-v7.js
@@ -3,7 +3,7 @@ var spiralComplete = false;
 
 const wavingHand = document.getElementById("waving-hand");
 
-gsap.fromTo("#helloSpiralPath", {drawSVG: "0 0%"}, {duration: 1.5, drawSVG: "0% 100%", onComplete: ()=> {
+gsap.fromTo("#helloSpiralPath", {drawSVG: "0 0%"}, {duration: 1.25, drawSVG: "0% 100%", onComplete: ()=> {
     // buckleText.classList.add("buckle-active");
     wavingHand.classList.add("popUp-wave-ani-group");
 } });
