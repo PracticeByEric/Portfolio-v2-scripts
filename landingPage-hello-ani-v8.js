f6e74b038354633b2e50a3e6d1bb0fe0a2a1aa3c
Eric Jiahuan Cheng <jc11769@nyu.edu>
2023-12-09 15:25:03 -0500

    v8 added delay

--- landingPage-hello-ani-v8.js
+++ landingPage-hello-ani-v8.js
@@ -3,7 +3,7 @@ var spiralComplete = false;
 
 const wavingHand = document.getElementById("waving-hand");
 
-gsap.fromTo("#helloSpiralPath", {drawSVG: "0 0%"}, {duration: 1.25, drawSVG: "0% 100%", onComplete: ()=> {
+gsap.fromTo("#helloSpiralPath", {drawSVG: "0 0%"}, {duration: 1.25, drawSVG: "0% 100%", delay:1, onComplete: ()=> {
     // buckleText.classList.add("buckle-active");
     wavingHand.classList.add("popUp-wave-ani-group");
 } });
